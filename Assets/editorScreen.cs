6ac2eb634f26f2996adaa827f8042a1a30815cbf
Luke Hylton <bcadren.games@gmail.com>
2015-10-21 21:51:18 -0400

    worked out how the UI works; rest of it should just be typing

--- Assets/editorScreen.cs
+++ Assets/editorScreen.cs
@@ -8,14 +8,17 @@ public class editorScreen : MonoBehaviour {
     private Camera mainCamera;
     private screenMath math;
 
+    public int TileSize = 16;
+
     public int height = 15;
     public int width = 16;
 
-    public Vector2 lowerLeft;
-    public Vector2 upperRight;
+    private Vector2 lowerLeft;
+    private Vector2 upperRight;
 
+    public MeshCollider tileSetSelector;
     public index debugVar;
-    public int[] debugVar0;
+    public Collider debugVar0;
 
     void Start ()
 
@@ -37,16 +40,31 @@ public class editorScreen : MonoBehaviour {
             if ((Input.mousePosition.x > lowerLeft.x) && (Input.mousePosition.y > lowerLeft.y) && (Input.mousePosition.x < upperRight.x) && (Input.mousePosition.y < upperRight.y))
             {
                 index mousePosition = new index(Mathf.FloorToInt(((Input.mousePosition.x - lowerLeft.x)/(upperRight.x - lowerLeft.x)) * width), Mathf.FloorToInt(((Input.mousePosition.y - lowerLeft.y)/(upperRight.y-lowerLeft.y)) * height));
-                debugVar = mousePosition;
                 editUV(mousePosition, new Vector2[4] { new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 0), new Vector2(1, 1)});
             }
+
+            else
+            {
+                RaycastHit hit;
+
+                if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out hit))
+                {
+                    if (hit.collider.name == tileSetSelector.name)
+                    {
+                        int localWidth = tileSetSelector.gameObject.GetComponent<MeshRenderer>().material.mainTexture.width/TileSize;
+
+                        debugVar = Mathf. (hit.textureCoord);
+                    }
+                }
+
+            }
+
         }
 	}
 
     void editUV (index location, Vector2[] newUVs)
     {
         int[] activeSpace = math.lookup(location);
-        debugVar0 = activeSpace;
         Vector2[] tempUVs = screen.uv;
         for (int i = 0; i < activeSpace.Length; i++)
         {
